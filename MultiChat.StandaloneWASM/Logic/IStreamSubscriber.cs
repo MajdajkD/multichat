343c754b4eeccb05c164575970be9d9047d580b6
Daniel Majdajk <majdajkdaniel@msn.com>
2024-02-10 05:42:27 +0100

    replace code with dist

--- MultiChat.StandaloneWASM/Logic/IStreamSubscriber.cs
+++
@@ -1,9 +0,0 @@
-﻿using MultiChat.StandaloneWASM.Logic.Model;
-
-namespace MultiChat.StandaloneWASM.Logic
-{
-  public interface IStreamSubscriber
-  {
-    public event Func<ChatMessage, Task>? OnNewMessage;
-  }
-}
